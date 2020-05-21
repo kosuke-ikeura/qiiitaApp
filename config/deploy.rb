e1902093feeb233eaab6d3b14531122c80ed4b22
EC2 Default User <ec2-user@ip-10-0-1-55.ap-northeast-1.compute.internal>
2020-05-21 06:21:24 +0000

    first commit

---
+++ config/deploy.rb
@@ -0,0 +1,43 @@
+lock "3.8.1"
+
+set :application, "qiitaApp"
+set :repo_url, "git@github.com:YOUR_GITHUB_ACCOUNT/qiitaApp.git"
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.3.3'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all
+
+set :log_level, :warn 
+
+# Default value for :linked_files is []
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# Default value for linked_dirs is []
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for keep_releases is 5
+set :keep_releases, 3
+
+set :unicorn_pid, "#{shared_path}/tmp/pids/unicorn.pid"
+
+set :unicorn_config_path, -> { File.join(current_path, "config", "unicorn.rb") }
+
+namespace :deploy do
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+end
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end    
